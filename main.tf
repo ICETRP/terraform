813ce41221d035359847016e375716f1a0b19ad7
Manish <manish.6octo@gmail.com>
2025-12-08 22:16:17 +0800

    FIX: HCL syntax in main.tf + rename service connection to AzureRM for auth

--- main.tf
+++ main.tf
@@ -1,14 +1,16 @@
 terraform {
   required_version = ">= 1.9.0"
+
   required_providers {
-    azurerm = { source = "hashicorp/azurerm"; version = "~> 4.0" }
-    tls = { source = "hashicorp/tls"; version = "~> 4.0" }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
+
   backend "azurerm" {}  # Pipeline configures this
 }
-
-provider "azurerm" {
-  features {}
-}
-
-# ... your VM resources unchanged ...
