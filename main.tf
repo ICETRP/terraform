8f8838db9fd5a3ba29f8f0face2542ef35ffa949
Manish <manish.6octo@gmail.com>
2025-12-09 22:53:56 +0800

    Add Ubuntu VM Terraform and pipeline configuration

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# main.tf - Ubuntu VM Deployment
 terraform {
   required_version = ">= 1.9.0"
 
@@ -13,76 +14,199 @@ terraform {
   }
 }
 
+# Provider Configuration - uses Azure CLI authentication
 provider "azurerm" {
   features {}
+  
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  
+  # Use Azure CLI for authentication (requires az login)
+  use_cli = true
 }
 
+# Variables (will be set by pipeline)
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "ubuntu-vm-rg"
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "vm_name" {
+  type    = string
+  default = "ubuntu-vm"
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_B2s"  # ~$15/month
+}
+
+variable "admin_username" {
+  type    = string
+  default = "azureuser"
+}
+
+# Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "tf-win-rg"
-  location = "eastus"
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    environment = "production"
+    managed-by  = "terraform"
+  }
 }
 
+# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet"
+  name                = "${var.vm_name}-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+# Subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
+  name                 = "${var.vm_name}-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+# Network Security Group (allow SSH)
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.vm_name}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"  # Change to your IP for production
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate NSG with Subnet
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Public IP
 resource "azurerm_public_ip" "pip" {
-  name                = "vm-pip"
+  name                = "${var.vm_name}-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
+  
+  tags = {
+    environment = "production"
+  }
 }
 
+# Network Interface
 resource "azurerm_network_interface" "nic" {
-  name                = "vm-nic"
+  name                = "${var.vm_name}-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "ipconfig"
+    name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
-resource "tls_private_key" "ssh" {
+# Generate SSH key pair
+resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
+# Save SSH private key locally (in pipeline agent)
 resource "local_file" "private_key" {
-  content         = tls_private_key.ssh.private_key_pem
+  content         = tls_private_key.ssh_key.private_key_pem
   filename        = "${path.module}/azure-key.pem"
   file_permission = "0600"
 }
 
+# Save SSH public key locally
+resource "local_file" "public_key" {
+  content         = tls_private_key.ssh_key.public_key_openssh
+  filename        = "${path.module}/azure-key.pub"
+  file_permission = "0644"
+}
+
+# Ubuntu Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "ubuntu-vm"
+  name                = var.vm_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B2s"
-  admin_username      = "azureuser"
-  network_interface_ids = [azurerm_network_interface.nic.id]
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
 
+  # Disable password authentication, use SSH only
   disable_password_authentication = true
 
   admin_ssh_key {
-    username   = "azureuser"
-    public_key = tls_private_key.ssh.public_key_openssh
+    username   = var.admin_username
+    public_key = tls_private_key.ssh_key.public_key_openssh
   }
 
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  # Ubuntu 22.04 LTS
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
@@ -90,12 +214,54 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+  # Optional: Add custom data for cloud-init
+  custom_data = base64encode(<<-EOF
+    #!/bin/bash
+    echo "Custom script executed on first boot"
+    apt-get update
+    apt-get install -y nginx
+    systemctl enable nginx
+    systemctl start nginx
+  EOF
+  )
+
+  tags = {
+    environment = "production"
+    os          = "ubuntu"
+    version     = "22.04"
   }
 }
 
-output "public_ip" {
+# Outputs
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "vm_name" {
+  value = azurerm_linux_virtual_machine.vm.name
+}
+
+output "public_ip_address" {
   value = azurerm_public_ip.pip.ip_address
+}
+
+output "admin_username" {
+  value = azurerm_linux_virtual_machine.vm.admin_username
+}
+
+output "ssh_private_key_path" {
+  value = abspath(local_file.private_key.filename)
+}
+
+output "ssh_public_key" {
+  value     = tls_private_key.ssh_key.public_key_openssh
+  sensitive = true
+}
+
+output "ssh_command" {
+  value = "ssh -i azure-key.pem ${var.admin_username}@${azurerm_public_ip.pip.ip_address}"
+}
+
+output "nginx_url" {
+  value = "http://${azurerm_public_ip.pip.ip_address}"
 }
